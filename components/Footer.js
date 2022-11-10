d27413aa669da43bb1c9f4348092132ca251bf2e
Jaxon Adams <jadamsresume1@gmail.com>
2022-11-09 17:06:13 -0700

    footer created and styled

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>Jaxon Adams, 2022</footer>
+    );
+};
+
+export default Footer;
