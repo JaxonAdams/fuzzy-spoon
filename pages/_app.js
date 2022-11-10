05b4831790bacc89b80c972a96e4cc8533550f49
Jaxon Adams <jadamsresume1@gmail.com>
2022-11-09 17:20:27 -0700

    homepage media queries and footer fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,10 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className='wrapper'>
       <Header />
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
